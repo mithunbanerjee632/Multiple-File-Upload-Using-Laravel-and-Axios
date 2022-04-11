d57889e188739eb8133e3a6259f0f19d5c6bbc3c
Mithun Banerjee <34417291+mithunbanerjee632@users.noreply.github.com>
2022-04-11 21:00:00 +0600

    File Upload Mechanism

--- resources/js/upload.js
+++ resources/js/upload.js
@@ -13,10 +13,7 @@ $('.adBtn').on('click',function(){
 
       $('.fileList').append(tableRow);
 
-      //Remove Row
-    $('.CancelBtn').on('click',function (){
-        $(this).parents('tr').remove();
-    });
+
 
     $('.InputFile').on('change',function(){
         let myFile = $(this).prop('files');
@@ -24,6 +21,58 @@ $('.adBtn').on('click',function(){
         $(this).closest('tr').find('.fileSize').html(myFileSize+ "MB");
     });
 
+    //Upload button click
+    $('.UpBtn').on('click',function(event){
+        let myFile = $(this).closest('tr').find('.InputFile').prop('files');
+        let FileUpMb = $(this).closest('tr').find('.fileUpMB');
+        let FileUpPercent = $(this).closest('tr').find('.fileUpPercentage');
+        let FileStatus = $(this).closest('tr').find('.fileStatus');
+        let upBtn = $(this);
+        let formData = new FormData();
+        formData.append('fileKey',myFile[0]);
+
+        onFileUpload(formData,FileUpMb,FileUpPercent,FileStatus,upBtn);
+        event.preventDefault();  //bar bar excution off kora hoice...ekbar hobe
+        event.stopImmediatePropagation(); //j button e click kora hobe sudhu oitai run hobe baki gula off thakbe
+
+
+    });
+
+    //Remove Row
+    $('.CancelBtn').on('click',function (){
+        $(this).parents('tr').remove();
+    });
+
 
 
 })
+
+function onFileUpload(formData,FileUpMb,FileUpPercent,FileStatus,upBtn){
+    FileStatus.html('Loading...');
+    upBtn.prop('disabled',true);
+  let url = "/fileUpload";
+  let config = {
+      headers:{'content-type':'multipart/form-data'},
+      onUploadProgress:function (progressEvent){
+         let UpMb = (progressEvent.loaded/(1024*1024)).toFixed(2)+" MB";
+         let upPercent = ((progressEvent.loaded*100)/progressEvent.total).toFixed(2)+" %";
+          FileUpMb.html(UpMb);
+          FileUpPercent.html(upPercent);
+
+      }
+  }
+
+  axios.post(url,formData,config)
+      .then(function(response){
+         if(response.status==200){
+             FileStatus.html("Upload Success");
+             upBtn.prop('disabled',false);
+         }else{
+             FileStatus.html("Upload Failed!");
+             upBtn.prop('disabled',false);
+         }
+  }).catch(function(error){
+      FileStatus.html("Upload Failed!");
+      upBtn.prop('disabled',false);
+  });
+}
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UploadController;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,3 +17,5 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('Home');
 });
+
+Route::post('/fileUpload',[UploadController::class,'onUpload']);
